d5e4547c6325124d88b1cf23f5aa8dff6961961a
Jacob Kelley <exaybachay.ak@gmail.com>
2018-01-13 19:35:21 -0900

    Update content_script.js
    Added an entry that blocks the Reddit filler (they call it a spacer) which replaced the ads once they were gone.

--- content_script.js
+++ content_script.js
@@ -1,2 +1,4 @@
 var element = document.querySelector('div.organic-listing');
+var element2 = document.querySelector('class.listingsignupbar');
 element.parentElement.removeChild(element);
+element2.parentElement.removeChild(element2);
